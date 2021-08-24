3b230de6ab7b01b2490cd65783706867568530f2
jeonjonghyeok <tongher1685@gmail.com>
2021-08-24 18:26:13 +0900

    Edit IteamApplication

--- src/main/java/jjh/api/ItemApplication.java
+++ src/main/java/jjh/api/ItemApplication.java
@@ -1,5 +1,7 @@
 package jjh.api;
 
+import jjh.api.item.controller.ItemController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,9 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //개발순서: domain -> repository -> service -> controller
 @SpringBootApplication
 public class ItemApplication {
-
+	@Autowired
+	ItemController itemController;
 	public static void main(String[] args) {
 		SpringApplication.run(ItemApplication.class, args);
 	}
+
+
 
 }
